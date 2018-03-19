bc3261c49ff325293a9289f8053ec1868f7935c8
Ruben-HP\Ruben Santos <rubenf_santos@hotmail.com>
2018-03-19 15:57:27 +0000

    no message

---
+++ Exercicio 1/Calculator/Calculator/MainPage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Calculator
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ViewCell
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public static implicit operator Page(MainPage v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
